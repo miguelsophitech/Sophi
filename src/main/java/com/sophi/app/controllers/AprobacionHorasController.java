02e437a64b0cc6d82453cdc7eeb3d81ea97f79c9
DaveM-135 <67597482+DaveM-135@users.noreply.github.com>
2020-07-14 13:27:59 -0500

    Cambios Dave
    Cambios Dave

--- src/main/java/com/sophi/app/controllers/AprobacionHorasController.java
+++ src/main/java/com/sophi/app/controllers/AprobacionHorasController.java
@@ -2,7 +2,6 @@ package com.sophi.app.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import com.sophi.app.models.entity.Recurso;
 import com.sophi.app.models.entity.AprobacionHoras;
@@ -12,13 +11,11 @@ import com.sophi.app.models.service.IRecursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SessionAttributes("aprobacionhoras")
@@ -84,7 +81,7 @@ public class AprobacionHorasController {
             String tablaAprobacionHoras = "";
             tablaAprobacionHoras = "<tr><td><input type=\"checkbox\" name=\"check\" onClick=\"checkbox();\"/></td>";
             tablaAprobacionHoras += "<td>"+aprobacionhoras.getFecRegistro()+"</td>";
-            tablaAprobacionHoras += "<td>"+aprobacionhoras.getCodCliente()+"</td>";
+            //tablaAprobacionHoras += "<td>"+aprobacionhoras.getCodCliente()+"</td>";
             tablaAprobacionHoras += "<td>"+aprobacionhoras.getNombreRecurso()+"</td>";
             tablaAprobacionHoras += "<td>"+aprobacionhoras.getDescComentarioDetalle()+"</td>";
             tablaAprobacionHoras += "<td><input type=\"number\" name=\"validar\" th:value=\"${aprobacionhoras.getValDuracionValidada()}\" readonly=\"readonly\"/></td></tr>";
