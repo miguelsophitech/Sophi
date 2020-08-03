80fa2faad42821127d2bd405ea605d1a220ce3ab
DaveM-135 <67597482+DaveM-135@users.noreply.github.com>
2020-08-03 16:06:42 -0500

    Id modificado
    Id modificado

--- src/main/java/com/sophi/app/models/dao/ICapHoraDao.java
+++ src/main/java/com/sophi/app/models/dao/ICapHoraDao.java
@@ -10,11 +10,11 @@ import com.sophi.app.models.entity.CapHora;
 
 public interface ICapHoraDao extends CrudRepository<CapHora, Long>{
 	
-	@Query("FROM CapHora C WHERE C.fecInicioActividad = ?1 AND C.Id.codRecurso = ?2")
+	@Query("FROM CapHora C WHERE C.fecInicioActividad = ?1 AND C.codRecurso = ?2")
 	List<CapHora> findListCapHoraByFechaRecurso(Date fecha, Long codRecurso);
 	
 	
-	@Query("SELECT  SUM(valDuracionReportada) FROM CapHora C WHERE C.Id.codRecurso = ?1 AND C.fecInicioActividad  BETWEEN ?2 AND ?3")
+	@Query("SELECT  SUM(valDuracionReportada) FROM CapHora C WHERE C.codRecurso = ?1 AND C.fecInicioActividad  BETWEEN ?2 AND ?3")
 	Float findSumHorasReportadasSemana(Long codRecurso, Date fechaInicio, Date fechaFin);
 
 }
