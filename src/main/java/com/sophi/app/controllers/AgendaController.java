3bb23f4e62ca55f4d7c8350fcd3d15a79173b76a
DaveM-135 <67597482+DaveM-135@users.noreply.github.com>
2020-08-03 13:15:20 -0500

    Ver recurso
    Pantalla ver recurso modificado

--- src/main/java/com/sophi/app/controllers/AgendaController.java
+++ src/main/java/com/sophi/app/controllers/AgendaController.java
@@ -49,56 +49,6 @@ public class AgendaController {
 		model.addAttribute("clientes", listaClientes);
 		return "agenda";
 	}
-
-//	@RequestMapping(value = "/cargaContactos", method = RequestMethod.GET)
-//	@ResponseBody
-//	public String cargaContactos(@RequestParam("id") Long codCliente, Model model) {
-//		List<Agenda> agendaList;
-//
-//		if(codCliente == -1){
-//			agendaList = agendaService.findAll();
-//		} 
-//		
-//		else{
-//			agendaList = agendaService.findContactosBycodCliente(codCliente);
-//		}
-//
-//		String contenido = "";
-//		contenido = contenido + "<div class=\"table-responsive\">"+
-//		"<table class=\"table table-hover\" id=\"dataTable\" width=\"100%\" cellspacing=\"0\">"+
-//		"<thead>"+
-//		"<tr>"+
-//		"<th>Nombre</th>"+
-//        "<th>Puesto</th>"+
-//        "<th>E-mail</th>"+
-//        "<th>Tel&eacute;fono Celular</th>"+
-//		"<th>Tel&eacute;fono Empresa</th>"+
-//		"<th>Extensi&oacute;n</th>"+
-//		"<th>Acciones</th>"+
-//        "</tr>"+
-//		"</thead>"+
-//		"<tbody>";
-//		
-//		for(Agenda agenda:agendaList) {
-//			String tablaContactos = "";
-//			tablaContactos = "<tr><td>"+agenda.getDescContacto()+"</td>";
-//			tablaContactos += "<td>"+agenda.getDescPuesto()+"</td>";
-//			tablaContactos += "<td>"+agenda.getDescCorreoElectronico()+"</td>";
-//			tablaContactos += "<td>"+agenda.getDescTelCelular()+"</td>";
-//			tablaContactos += "<td>"+agenda.getDescTelEmpresa()+"</td>";
-//			tablaContactos += "<td>"+agenda.getDescTelExt()+"</td>";
-//			tablaContactos += "<td><div class=\"btn-group btn-group-sm\" role=\"group\">";
-//			tablaContactos += "<a type=\"button\" class=\"btn btn-info\" href=\"/dataContacto/"+agenda.getCodContacto()+"\" data-toggle=\"tooltip\" title=\"Ver\"><i class=\"far fa-address-book\"></i></a>";
-//			tablaContactos += "<a type=\"button\" class=\"btn btn-info\" href=\"/formContacto/"+agenda.getCodContacto()+"\" data-toggle=\"tooltip\" title=\"Editar\"><i class=\"far fa-edit\"></i></a>";
-//			tablaContactos += "<button type=\"button\" class=\"btn btn-danger\" onclick=\"eliminar("+agenda.getCodContacto()+");\" data-toggle=\"tooltip\" title=\"Borrar\"><i class=\"far fa-trash-alt\"></i></button></div></td></tr>";
-//			contenido += tablaContactos;
-//		}
-//		
-//		contenido = contenido + "</tbody>"+
-//        "</table>"+
-//        "</div>";
-//		return contenido;
-//	}
 	
 	@RequestMapping(value = "/dataContacto/{id}")
 	public String verContacto(@PathVariable(value = "id") Long codContacto, Map<String, Object> model, RedirectAttributes flash) {
